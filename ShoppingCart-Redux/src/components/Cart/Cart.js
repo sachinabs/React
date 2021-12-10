2aae1ca1d67b9ec514da7f6abc4417021f8f253a
sachinabs <sachinabs.js@gmail.com>
2021-12-10 17:14:54 +0530

    shoping site redux

---
+++ ShoppingCart-Redux/src/components/Cart/Cart.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import styles from "./Cart.module.css";
+
+import { connect } from "react-redux";
+
+import CartItem from "./CartItem/CartItem";
+
+const Cart = ({ cart }) => {
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+
+  useEffect(() => {
+    let items = 0;
+    let price = 0;
+
+    cart.forEach((item) => {
+      items += item.qty;
+      price += item.qty * item.price;
+    });
+
+    setTotalItems(items);
+    setTotalPrice(price);
+  }, [cart, totalPrice, totalItems, setTotalPrice, setTotalItems]);
+
+  function showAlert() {
+    alert("This will redirect to payment...!")
+  }
+  function showFalse() {
+    alert("Choose some Products...!")
+  }
+  return (
+    <div className={styles.cart}>
+      <div className={styles.cart__items}>
+        {cart.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
+      <div className={styles.cart__summary}>
+        <h4 className={styles.summary__title}>Cart Summary</h4>
+        <div className={styles.summary__price}>
+          <span>TOTAL: ({totalItems} items)</span>
+          <span>&#8377; {totalPrice}</span>
+        </div>
+        <button className={styles.summary__checkoutBtn} onClick={(totalItems===0)?showFalse:showAlert}>
+          Proceed To Checkout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    cart: state.shop.cart,
+  };
+};
+
+export default connect(mapStateToProps)(Cart);
